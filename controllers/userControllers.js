4841612f382dfcdf4bdfeb1e651b657c52e1f332
Manoj Hajam <manojhajam4@gmail.com>
2025-08-04 16:07:10 +0545

    HashedPassword using bcrypt

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,64 +1,69 @@
 import { userModel } from "../db.js";
-import jwt from "jsonwebtoken"
-const JWT_USER_PASSWORD = "aladld123"
+import jwt from "jsonwebtoken";
+const JWT_USER_PASSWORD = "aladld123";
+import bcrypt from "bcrypt";
 
-export const Signup =async (req, res) => {
-    const { email, password, lastName, firstName } = req.body;  // do ZOD validation
+export const Signup = async (req, res) => {
+  const { email, password, lastName, firstName } = req.body; // do ZOD validation
 
-    // TO do: Password Hashed
-    try {
-         await userModel.create({
-           email: email,
-           password: password,
-           lastName: lastName,
-           firstName: firstName,
-         });
-        res.json({
-          message: "Signup successed",
-        });
-    } catch (error) {
-        console.log(error)
-        res.json({
-            success: false,
-            message: error.message
-        })
-    }
-   
+  // TO do: Password Hashed
 
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-    
-}
+  try {
+    await userModel.create({
+      email: email,
+      password: hashedPassword,
+      lastName: lastName,
+      firstName: firstName,
+    });
+    res.json({
+      message: "Signup successed",
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
-export const Signin = async (req, res) => { 
-    const { email, password } = req.body;
-    
-    const user = await userModel.findOne({
-        email, email,
-        password, password
-    })
+export const Signin = async (req, res) => {
+  const { email, password } = req.body;
 
-    // ideally password should be hashed, and hence you can't compare the user provided passsword and the database password  
-    if (user) {
-        const token = jwt.sign({
-            id: user._id
-        }, JWT_USER_PASSWORD)
+  const user = await userModel.findOne({
+    email,
+    email,
+    password,
+    password,
+  });
 
-        res.json({
-            token: token
-        })
-    } else {
-        res.status(403).json({
-            message: "Incorrect credentials"
-        })
-    }
+  // ideally password should be hashed, and hence you can't compare the user provided passsword and the database password
+  if (user) {
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      JWT_USER_PASSWORD
+    );
 
-    //Do cookie login instead of token based in future
+    res.json({
+      token: token,
+    });
+  } else {
+    res.status(403).json({
+      message: "Incorrect credentials",
+    });
+  }
 
-
-}
+  //Do cookie login instead of token based in future
+};
 
 export const Purchase = (req, res) => {
-    res.json({
-        message: "Purchase endpoint"
-    });
-}
+  res.json({
+    message: "Purchase endpoint",
+  });
+};
