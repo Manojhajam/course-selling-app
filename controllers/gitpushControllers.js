b552eb0c283661221b7726b09d1719bff6357692
Manoj Hajam <manojhajam4@gmail.com>
2025-09-21 19:40:49 +0545

    just push

---
+++ controllers/gitpushControllers.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const gitpushControllers = () => {
+  return (
+    <div>
+      <h1>This is git push controllers</h1>
+    </div>
+  )
+}
+
+export default gitpushControllers
